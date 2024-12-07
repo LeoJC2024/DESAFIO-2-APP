12b3f891af7b50da563d95e02a20cf41440f7604
LeoJC2024 <leojc20@hotmail.com>
2024-12-06 22:06:05 -0300

    Add files via upload
    MY-APP

---
+++ App.js
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import { Routers } from './routes';
+export default function App() {
+  return (
+    <Routers/>
+  );
+}
+
+
+//import { StatusBar } from 'expo-status-bar';
+//import { Text, View, StyleSheet } from 'react-native';
+
+//import Login from './src/screens/Login';
+
+//export default function App() {
+  //return (
+    //<View style = {style.container}> {//add style = {stule.container} para indicar tamanho da tela
+      //}          
+      //<StatusBar style="auto" />
+      //<Login />
+    //</View>
+  //);
+//}
+// criado style para o container, para centralizar componentes
+//const style = StyleSheet.create({
+  //container: {
+    //flex: 1,
+    //outra maneira para centralizar icones dentro da tela 
+    //width:'100%',
+    //height:'100%',
+  //}
+//})
+
